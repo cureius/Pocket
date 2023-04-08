38cf9172f215491730d5ff50554e5e66c3c9592a
Souraj Pal <souraj.pal@godeskless.com>
2023-04-08 10:04:35 +0530

    PH-1 (clean up)

--- app/src/main/java/com/cureius/pocket/MainActivity.kt
+++ app/src/main/java/com/cureius/pocket/MainActivity.kt
@@ -1,28 +1,16 @@
 package com.cureius.pocket
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.filled.*
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.rememberNavController
 import com.cureius.pocket.feature_navigation.presentation.NavigationController
-import com.cureius.pocket.feature_sms_sync.util.SyncUtils
-import com.cureius.pocket.feature_transaction.domain.model.Transaction
-import com.cureius.pocket.feature_transaction.presentation.add_transaction.AddTransactionEvent
-import com.cureius.pocket.feature_transaction.presentation.add_transaction.AddTransactionViewModel
 import com.cureius.pocket.ui.theme.PocketTheme
-import com.cureius.pocket.util.SharedPreferencesUtil
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
